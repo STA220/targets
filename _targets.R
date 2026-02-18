c5fca7a47716d92cafb80239599ff7487816f2b9
Erik Bülow <eriklgb@gmail.com>
2026-02-18 08:46:06 +0100

    simplify

--- _targets.R
+++ _targets.R
@@ -4,45 +4,18 @@ tar_source()
 
 tar_option_set(
   format = "qs",
-  packages = c(
-    "dplyr",
-    "janitor",
-    "plotly",
-    "NHANES"
-  )
+  packages = c("dplyr", "janitor")
 )
 
 list(
-  # * Input data ----
-  tar_target(
-    # Target name
-    local_file,
-    # Path
-    "data/NHANES.csv",
-    # * This tells targets that this string is a file
-    format = "file"
-  ),
-  tar_target(
-    tbl_NHANES,
-    {
-      readr::read_csv(local_file)
-    }
-  ),
+  tar_target(local_file, "data/NHANES.csv", format = "file"),
+  tar_target(tbl_NHANES, readr::read_csv(local_file)),
 
   # * Subset ----
-  tar_target(
-    tbl_NHANES_subset,
-    {
-      tbl_NHANES |>
-        select(one_of(fn_cols()))
-    }
-  ),
+  tar_target(tbl_NHANES_subset, select(tbl_NHANES, one_of(fn_cols()))),
 
   # * Cleanup ----
-  tar_target(
-    tbl_clean,
-    fn_clean(tbl_NHANES_subset)
-  ),
+  tar_target(tbl_clean, fn_clean(tbl_NHANES_subset)),
 
   # * Model ----
   tar_target(mod_lm, fn_lm_mod(tbl_clean)),
@@ -53,25 +26,12 @@ list(
     export_mod_lm_coeffs,
     {
       output_file <- "data/mod_lm_coeffs.csv"
-
-      mod_lm_coeffs |>
-        readr::write_csv(output_file)
-
+      readr::write_csv(mod_lm_coeffs, output_file)
       output_file
     },
-    format = "file" # This tells targets that this string is a file
+    format = "file"
   ),
 
   # * Report ----
-  tar_render(
-    report,
-    path = "report.qmd",
-    params = list(
-      tbl_coeff = mod_lm_coeffs
-    )
-  )
-
-  # Exercise ----
-  # Don't forget to add a "," after the last target ^^
-  # Your code here....
+  tar_render(report, "report.qmd", params = list(tbl_coeff = mod_lm_coeffs))
 )
